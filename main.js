9950130d81d49649bdb15e4ade0bfb8aa8a33f92
dhiyashah <dhiyashah2005@gmail.com>
2022-07-17 23:23:12 -0600

    Update

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("hi");
+// Variables for HTML
+let containerE1 = document.getElementById("container");
+let inputE1 = document.getElementById("FName");
+let inputE2 = document.getElementById("LName");
+let randBtn = document.getElementById("randNickN");
+let allBtn = document.getElementById("allNickN");
+
+randBtn.addEventListener("click", randomNickname);
+allBtn.addEventListener("click", allNicknames);
