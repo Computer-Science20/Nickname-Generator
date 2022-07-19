e5b995efe8aadaa8b477a5aec342577c4aceab06
dhiyashah <dhiyashah2005@gmail.com>
2022-07-19 11:33:43 -0600

    Update

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Variables for HTML
-let containerEl = document.getElementById("container");
-let inputEl = document.getElementById("FName");
-let input2El = document.getElementById("LName");
+let containerE1 = document.getElementById("container");
+let inputE1 = document.getElementById("FName");
+let inputE2 = document.getElementById("LName");
 let allBtn = document.getElementById("allNickN");
 let randBtn = document.getElementById("randNickN");
 let nickname;
 
-document.addEventListener("click", allBtn);
-document.addEventListener("click", randBtn);
+allBtn.addEventListener("click", allNickNGenerator);
+randBtn.addEventListener("click", randNickNGenerator);
 
 fetch("nickname.txt").then(convertData).then(processData);
 
@@ -19,19 +19,22 @@ function processData(stringData){
     nickname = stringData.split(/\r?\n/);
 }
 
-function allBtn() {
-    let firstName = inputEl.value;
-    let lastName = input2El.value;
+function allNickNGenerator() {
+    let firstName = inputE1.value;
+    let lastName = inputE2.value;
     let divStr = "";
     for (let i = 0; i < nickname.length; i++);
-    divStr += `<div> ${firstName} "${nickname[i]}" ${lastName} </div>`;
-    containerEl.innerHTML = divStr
+    divStr += `<div> ${firstName} "${nickname[1]}" ${lastName} </div>`;
+    containerE1.innerHTML = divStr
 }
 
-function randBtn() {
-    let firstName = inputEl.value;
-    let lastName = input2El.value;
+function randNickNGenerator() {
+    let firstName = inputE1.value;
+    let lastName = inputE2.value;
     let randName = randomElement(nickname);
-    for (let i = 0; i < nickname.length; i++);
-    containerEl.innerHTML = `<div> ${firstName} "${randName}" ${lastName} </div>`;
+    containerE1.innerHTML = `<div> ${firstName} "${randName}" ${lastName} </div>`;
+}
+
+function randomElement(anArray) {
+    return anArray[randomInt(0, anArray.length)]
 }
